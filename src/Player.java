10e146e3fc65ac30c84f919b9d256b8b0ceccdb2
josephkjohnson04 <josephkjohnson04@gmail.com>
2024-02-07 09:30:33 -0600

    Added initial comments

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * The Player logic for the High/Low App that allows for two players to play the game
+ * Each player will enter their name and begin guessing as to the correct number in the game.
+ * Their total wins, max attempts, and lowest attempts for each question are stored to be later displayed.
+ */
