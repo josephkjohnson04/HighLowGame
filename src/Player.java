16c6806a32d110cb32d04a6b1eef2e6e3cc27dc2
Jkile <jaylenkile@gmail.com>
2024-02-07 09:48:27 -0600

    Started making stuff

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,28 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
+ * @author Joe Johnson and Jaylen Kile
+ * @version 1.0
+ * @since 2/7/2024
  * The Player logic for the High/Low App that allows for two players to play the game
  * Each player will enter their name and begin guessing as to the correct number in the game.
  * Their total wins, max attempts, and lowest attempts for each question are stored to be later displayed.
  */
+
+public class Player {
+
+    private int highestAttempts;
+
+    private int id;
+
+    private static int lastNumber;
+
+    private int lowestAttempts;
+
+    private java.lang.String name;
+
+    private int wins;
+
+
+
+}
