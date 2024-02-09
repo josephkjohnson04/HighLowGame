24fec67f90cb8800dbc695bb4788e3768bc9c809
josephkjohnson04 <josephkjohnson04@gmail.com>
2024-02-09 09:22:38 -0600

    Added additional logic and comments to Player ints

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,10 @@
-import java.util.Random;
 import java.util.Scanner;
 
 /**
  * @author Joe Johnson and Jaylen Kile
  * @version 1.0
  * @since 2/7/2024
+ * @see <a href="https://github.com/josephkjohnson04/HighLowGame.git">...</a>
  * The Player logic for the High/Low App that allows for two players to play the game
  * Each player will enter their name and begin guessing as to the correct number in the game.
  * Their total wins, max attempts, and lowest attempts for each question are stored to be later displayed.
@@ -12,18 +12,41 @@ import java.util.Scanner;
 
 public class Player {
 
-    private int highestAttempts;
+    /**
+     * Input scanner for all user's inputs
+     */
+    private static Scanner sc = new Scanner(System.in);
+
+    /**
+     *
+     */
+    private static int lastNumber = 0;
 
     private int id;
 
-    private static int lastNumber;
+    private java.lang.String name;
+
+    public Player() {
+        this.id = ++Player.lastNumber;
+        this.name = "Unknown";
+    }
+
+    public Player(String name) {
+        this();
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public static int getLastNumber() {
+        return Player.lastNumber;
+    }
+
+    private int highestAttempts;        // Player's highest attempts to guess the correct number
 
     private int lowestAttempts;
 
-    private java.lang.String name;
-
     private int wins;
-
-
 
 }
