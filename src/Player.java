a8e9836baaef6a0c3d2034fbcc404466a76ea37a
josephkjohnson04 <josephkjohnson04@gmail.com>
2024-02-12 09:21:54 -0600

    Finished all fields and added comments

--- src/Player.java
+++ src/Player.java
@@ -17,33 +17,62 @@ public class Player {
      */
     private java.lang.String name;
 
-
     /**
-     * Class field that is shared across all player objects that tracks the last unique player id used. It is increased
-     * by one and assigned as the player's unique id by the default constructor.
+     * Class field that is shared across all player objects that tracks the last unique player id used. It is
+     * increased by one and assigned as the player's unique id by the default constructor.
      */
     private static int lastNumber = 0;
 
     /**
-     * Player's unique id that is automatically assigned by rhe default constructor.
+     * Player's unique id that is automatically assigned by the default constructor.
      */
     private int id;
 
+    /**
+     * The player's highest attempt to guess the correct number across all rounds
+     */
+    private int highestAttempts;
 
+    /**
+     * The player's lowest attempt to guess the correct number across all rounds
+     */
+    private int lowestAttempts;
+
+    /**
+     * The player's total wins across all games played
+     */
+    private int wins;
+
+    /**
+     * Creates a new player with a unique id, sets their name to unknown, and increments the class field lastNumber
+     * by one
+     */
     public Player() {
         this.id = ++Player.lastNumber;
         this.name = "Unknown";
     }
 
+    /**
+     * Overload constructor creates a new player and calls the default constructor, then sets name
+     * @param name
+     */
     public Player(String name) {
         this();
         this.name = name;
     }
 
+    /**
+     * Gets and returns the player's name
+     * @return
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the player's name unless empty and removes trailing spaces
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
@@ -51,12 +80,6 @@ public class Player {
     public static int getLastNumber() {
         return Player.lastNumber;
     }
-
-    private int highestAttempts;        // Player's highest attempts to guess the correct number
-
-    private int lowestAttempts;
-
-    private int wins;
 
 }
 
